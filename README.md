b976e8bef3c3816aed05b96be684829494183c80
BorisStankov98 <boris.stankovv@gmail.com>
2024-10-01 17:32:44 +0300

    re-done the whole admin app

--- README.md
+++ README.md
@@ -1,29 +1,70 @@
----
-page_type: sample
-languages:
-- javascript
-products:
-- azure
-- azure-communication-services
----
+# Getting Started with Create React App
 
-# Add 1 on 1 video calling to your application
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-This code sample walks through the process of integration Azure Communication Services video calling into your JavaScript application.
+## Available Scripts
 
-This quickstart sample includes the code that is explained as part of [this document](https://docs.microsoft.com/azure/communication-services/quickstarts/voice-video-calling/get-started-with-video-calling). See that document for additional details on how this sample works.
+In the project directory, you can run:
 
-## Prerequisites
-- Obtain an Azure account with an active subscription. [Create an account for free](https://azure.microsoft.com/free/?WT.mc_id=A261C142F).
-- You need to have [Node.js 18](https://nodejs.org/dist/v18.18.0/). You can use the msi installer to install it.
-- Create an active Communication Services resource. [Create a Communication Services resource](https://docs.microsoft.com/azure/communication-services/quickstarts/create-communication-resource).
-- Create a User Access Token to instantiate the call client. [Learn how to create and manage user access tokens](https://docs.microsoft.com/azure/communication-services/quickstarts/access-tokens?pivots=programming-language-javascript).
+### `npm start`
 
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-## Run the code
-1. Run `npm i` on the directory of the project to install dependencies
-2. Use the webpack serve command to build and run the app on a local server:
-`npx webpack serve --config webpack.config.js`
-3. Once the local server starts up, open your browser and navigate to http://localhost:8080/. You'll see the calling application:
-![Render of sample application](../media/1-on-1-video-calling.png)
-To make an 1:1 outgoing video call, first provide user access token to initiate the call agent, then provide a ACS user ID in the text field and clicking the Start Call button. When the callee answers the call with video, call will be connected and you will see each other's videos.
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+
+If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+
+You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
---
+++ src/App.js
@@ -0,0 +1,333 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+const { CallClient, VideoStreamRenderer, LocalVideoStream } = require('@azure/communication-calling');
+const { AzureCommunicationTokenCredential } = require('@azure/communication-common');
+
+
+let callAgent;
+let deviceManager;
+let call;
+let incomingCall;
+let localVideoStream;
+let localVideoStreamRenderer;
+
+let remoteVideosGallery;
+let localVideoContainer;
+
+let USER_ACCESS_TOKEN_ADMIN = ""
+let USER_CALLE_ID = ""
+
+function App() {
+
+  function fetchTokenFromGitHub() {
+    return axios.get("https://api.github.com/repos/BiekeSurfOrg/acess-tokens/contents/acess-tokens.json", {
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28"
+      }
+    }).then((result => {
+
+      const { caller, callee } = JSON.parse(atob(result.data.content).toString())
+      USER_ACCESS_TOKEN_ADMIN = callee.userAcessToken
+    }
+    ))
+  }
+
+  const [isCameraEnabled, setEnableCamera] = useState(false);
+  const [isIncomingCallDisabled, setIncomingCallDisabledButtonDissable] = useState(true);
+
+
+  useEffect(() => {
+    remoteVideosGallery = document.getElementById('remoteVideosGallery');
+    localVideoContainer = document.getElementById('localVideoContainer');
+    init();
+  }, []);
+
+  const init = async () => {
+    try {
+      await fetchTokenFromGitHub()
+      console.log(USER_ACCESS_TOKEN_ADMIN)
+      const callClient = new CallClient();
+      const tokenCredential = new AzureCommunicationTokenCredential(USER_ACCESS_TOKEN_ADMIN);
+      callAgent = await callClient.createCallAgent(tokenCredential)
+      // Set up a camera device to use.
+      deviceManager = await callClient.getDeviceManager();
+      await deviceManager.askDevicePermission({ video: true });
+      await deviceManager.askDevicePermission({ audio: true });
+      // Listen for an incoming call to accept.
+      callAgent.on('incomingCall', async (args) => {
+        try {
+          incomingCall = args.incomingCall;
+          setIncomingCallDisabledButtonDissable(false);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const startVideoCall = async () => {
+    try {
+      const localVideoStream = await createLocalVideoStream();
+      console.log(localVideoStream);
+      setEnableCamera(true)
+      const videoOptions = localVideoStream ? { localVideoStreams: [localVideoStream] } : undefined;
+      call = callAgent.startCall([{ communicationUserId: USER_CALLE_ID }], { videoOptions });
+      // Subscribe to the call's properties and events.
+      subscribeToCall(call);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const acceptCall = async () => {
+    try {
+      const localVideoStream = await createLocalVideoStream();
+      setIncomingCallDisabledButtonDissable(true);
+      setEnableCamera(true)
+      console.log(isIncomingCallDisabled, "accept call");
+
+      const videoOptions = localVideoStream ? { localVideoStreams: [localVideoStream] } : undefined;
+      call = await incomingCall.accept({ videoOptions });
+      // Subscribe to the call's properties and events.
+      subscribeToCall(call);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const subscribeToCall = (call) => {
+    try {
+      // Inspect the initial call.id value.
+      console.log(`Call Id: ${call.id}`);
+      //Subscribe to call's 'idChanged' event for value changes.
+      call.on('idChanged', () => {
+        console.log(`Call Id changed: ${call.id}`);
+      });
+
+      // Inspect the initial call.state value.
+      console.log(`Call state: ${call.state}`);
+      // Subscribe to call's 'stateChanged' event for value changes.
+      call.on('stateChanged', async () => {
+        console.log(`Call state changed: ${call.state}`);
+        if (call.state === 'Connected') {
+        } else if (call.state === 'Disconnected') {
+          console.log(`Call ended, call end reason={code=${call.callEndReason.code}, subCode=${call.callEndReason.subCode}}`);
+        }
+      });
+
+      call.on('isLocalVideoStartedChanged', () => {
+        console.log(`isLocalVideoStarted changed: ${call.isLocalVideoStarted}`);
+      });
+      console.log(`isLocalVideoStarted: ${call.isLocalVideoStarted}`);
+      call.localVideoStreams.forEach(async (lvs) => {
+        localVideoStream = lvs;
+        await displayLocalVideoStream();
+      });
+      call.on('localVideoStreamsUpdated', e => {
+        e.added.forEach(async (lvs) => {
+          localVideoStream = lvs;
+          await displayLocalVideoStream();
+        });
+        e.removed.forEach(lvs => {
+          removeLocalVideoStream();
+        });
+      });
+
+      // Inspect the call's current remote participants and subscribe to them.
+      call.remoteParticipants.forEach(remoteParticipant => {
+        subscribeToRemoteParticipant(remoteParticipant);
+      });
+      // Subscribe to the call's 'remoteParticipantsUpdated' event to be
+      // notified when new participants are added to the call or removed from the call.
+      call.on('remoteParticipantsUpdated', e => {
+        // Subscribe to new remote participants that are added to the call.
+        e.added.forEach(remoteParticipant => {
+          subscribeToRemoteParticipant(remoteParticipant)
+        });
+        // Unsubscribe from participants that are removed from the call
+        e.removed.forEach(remoteParticipant => {
+          console.log('Remote participant removed from the call.');
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const subscribeToRemoteParticipant = (remoteParticipant) => {
+    try {
+      // Inspect the initial remoteParticipant.state value.
+      console.log(`Remote participant state: ${remoteParticipant.state}`);
+      // Subscribe to remoteParticipant's 'stateChanged' event for value changes.
+      remoteParticipant.on('stateChanged', () => {
+        console.log(`Remote participant state changed: ${remoteParticipant.state}`);
+      });
+
+      // Inspect the remoteParticipants's current videoStreams and subscribe to them.
+      remoteParticipant.videoStreams.forEach(remoteVideoStream => {
+        subscribeToRemoteVideoStream(remoteVideoStream)
+      });
+      // Subscribe to the remoteParticipant's 'videoStreamsUpdated' event to be
+      // notified when the remoteParticiapant adds new videoStreams and removes video streams.
+      remoteParticipant.on('videoStreamsUpdated', e => {
+        // Subscribe to new remote participant's video streams that were added.
+        e.added.forEach(remoteVideoStream => {
+          subscribeToRemoteVideoStream(remoteVideoStream)
+        });
+        // Unsubscribe from remote participant's video streams that were removed.
+        e.removed.forEach(remoteVideoStream => {
+          console.log('Remote participant video stream was removed.');
+        })
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const subscribeToRemoteVideoStream = async (remoteVideoStream) => {
+    let renderer = new VideoStreamRenderer(remoteVideoStream);
+    let view;
+    let remoteVideoContainer = document.createElement('div');
+    remoteVideoContainer.className = 'remote-video-container';
+
+    let loadingSpinner = document.createElement('div');
+    loadingSpinner.className = 'loading-spinner';
+    remoteVideoStream.on('isReceivingChanged', () => {
+      try {
+        if (remoteVideoStream.isAvailable) {
+          const isReceiving = remoteVideoStream.isReceiving;
+          const isLoadingSpinnerActive = remoteVideoContainer.contains(loadingSpinner);
+          if (!isReceiving && !isLoadingSpinnerActive) {
+            remoteVideoContainer.appendChild(loadingSpinner);
+          } else if (isReceiving && isLoadingSpinnerActive) {
+            remoteVideoContainer.removeChild(loadingSpinner);
+          }
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
+
+    const createView = async () => {
+      // Create a renderer view for the remote video stream.
+      view = await renderer.createView();
+      // Attach the renderer view to the UI.
+      console.log(remoteVideoContainer, "Remote video container");
+
+      remoteVideoContainer.appendChild(view.target);
+      remoteVideosGallery.appendChild(remoteVideoContainer);
+    }
+
+    // Remote participant has switched video on/off
+    remoteVideoStream.on('isAvailableChanged', async () => {
+      try {
+        if (remoteVideoStream.isAvailable) {
+          await createView();
+        } else {
+          view.dispose();
+          remoteVideosGallery.removeChild(remoteVideoContainer);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
+
+    // Remote participant has video on initially.
+    if (remoteVideoStream.isAvailable) {
+      try {
+        await createView();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+
+
+  const stopVideoInCall = async () => {
+    try {
+      await call.stopVideo(localVideoStream);
+      setEnableCamera(false)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const startVideoInCall = async () => {
+    try {
+      const localVideoStream = await createLocalVideoStream();
+      await call.startVideo(localVideoStream);
+      setEnableCamera(true)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  const createLocalVideoStream = async () => {
+    const camera = (await deviceManager.getCameras())[0];
+    if (camera) {
+      return new LocalVideoStream(camera);
+    } else {
+      console.error(`No camera device found on the system`);
+    }
+  }
+
+  /**
+   * Display your local video stream preview in your UI
+   */
+  const displayLocalVideoStream = async () => {
+    try {
+      localVideoStreamRenderer = new VideoStreamRenderer(localVideoStream);
+      const view = await localVideoStreamRenderer.createView();
+      localVideoContainer.appendChild(view.target);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /**
+   * Remove your local video stream preview from your UI
+   */
+  const removeLocalVideoStream = async () => {
+    try {
+      localVideoStreamRenderer.dispose();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const hangUpVideoCall = async () => {
+    await call.hangUp();
+    setIncomingCallDisabledButtonDissable(false)
+
+  }
+
+  return (
+    <div className='main-content'>
+      <h1>Holopod Teams Integration Admin</h1>
+      <div className='video-container-holder'>
+        <div className='video-container'>
+          <div id="localVideoContainer" >You:</div>
+        </div>
+        <div className='video-container'>
+          <div id="remoteVideosGallery" >Holopod User:</div>
+        </div>
+      </div>
+      <div className='buttons'>
+        <button className={isIncomingCallDisabled ? 'kbc-style-button button-dissabled' : 'kbc-style-button'} id="accept-call-button" type="button" onClick={() => acceptCall()} >Accept Call</button>
+        <button className={!isIncomingCallDisabled ? 'kbc-style-button button-dissabled' : 'kbc-style-button'} id="hangup-call-button" type="button" onClick={() => hangUpVideoCall()} >Hang up Call</button>
+        <button className={isCameraEnabled ? 'kbc-style-button button-dissabled' : 'kbc-style-button'} id="start-video-button" type="button" onClick={() => startVideoInCall()}>Start Video</button>
+        <button className={!isCameraEnabled ? 'kbc-style-button button-dissabled' : 'kbc-style-button'} type="button" onClick={() => stopVideoInCall()}>Stop Video</button>
+      </div>
+
+      <script src="./main.js"></script>
+    </div>
+  );
+}
+
+
+export default App;
